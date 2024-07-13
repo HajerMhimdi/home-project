c9bb47a9c5001d22e807ab54a36c245b825a19ea
hajer <hajer.mhimdi@gmail.com>
2024-07-13 11:52:57 +0100

    Add routes to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,23 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { MatButtonModule } from '@angular/material/button';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
+import { RouterModule } from '@angular/router';
+import routeConfig from './routes';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HousingLocationComponent
+    HousingLocationComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule.forRoot(routeConfig)
     
 
     
